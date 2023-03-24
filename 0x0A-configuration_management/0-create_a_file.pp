afd32f610d37852caf0e53ea787d94ee27e5e110
3SSI3 <starsm004@gmail.com>
2023-03-24 23:34:17 +0300

    Fixed :hammer_and_wrench::

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-# Creates a file
-file { '/tmp/school':
+# creates a file in tmp
+
+file {'/tmp/school':
+	path	=> '/tmp/school',
 	mode	=> '0744',
 	owner	=> 'www-data',
 	group	=> 'www-data',
-	content => 'I love Puppet'
+	content	=> 'I love Puppet'
 }
 
