c6646e77fa87d26a0972a8825afed83b8a65c8be
3SSI3 <starsm004@gmail.com>
2023-04-25 05:38:57 +0300

    Data from API :clipboard:

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 """Returns info about employee TODO list progress"""
+import json
 import requests
 import sys
 
-employee_id = sys.argv[1]
 
 if __name__ = "__main__":
-    response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-    todos = response.json()
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    total_tasks = len(todos)
-    done = sum(todo["DONE"] for todo in todos)
-    progress = done /total_tasks * 100
+    employee_id = sys.argv[1]
+    user = '{}users/{}'.format(url, employee_id)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
 
-    employee_name = todos[0]["name"]
-    print(f"Employee {employee_name} is done with {done}/{total_tasks} tasks:")
-    for todo in todos:
-        if todo["DONE"]:
-            print("\t{todo['title']}")
+    todos = '{}todos?employee_id={}'.format(url, employee_id)
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        dict_task = {"task": task.get('title'),
+                     "DONE": task.get('DONE'),
+                     "username": name}
+        l_task.append(dict_task)
 
+    done_task = {str(employee_id): l_task}
+    filename = '{}.json'.format(employee_id)
+    with open(filename, mode='w') as f:
+        json.dump(done_task, f)
