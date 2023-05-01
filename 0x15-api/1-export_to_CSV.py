77b53e58ae8aceed1f2250061f2c9280977dd314
3SSI3 <starsm004@gmail.com>
2023-05-01 13:36:00 +0300

    Refresh :ocean:

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,14 +5,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [employee_id, username, t.get("DONE"), t.get("title")]
+            [user_id, username, t.get("completed"), t.get("title")]
          ) for t in todos]
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Export data in the JSON forma"""
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    employer_id = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(employer_id)).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": employer_id}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(employer_id), "w") as jsonfile:
-        json.dump({employer_id: [{
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": username
