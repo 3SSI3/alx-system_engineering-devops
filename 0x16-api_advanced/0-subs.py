dd6f5e03b5dcfb05699d06d5508661529e1956d3
3ASSI3 <starsm004@gmail.com>
2023-05-10 00:30:24 +0300

    Subs:abacus:

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+   function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    else:
+        return 0
+
