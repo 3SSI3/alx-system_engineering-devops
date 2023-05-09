76cdddc9af4c9eb6f9d6b89c615db1e623dc66ce
3SSI3 <starsm004@gmail.com>
2023-05-10 02:04:01 +0300

    Fixed Top ten:hammer:

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,9 +8,9 @@ import requests
 
 def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code == 404:
         data = response.json()['data']['children']
         for post in data:
             print(post['data']['title'])
