2ea02c2daa3f1c308fa787b08ff1fac2d58d6c79
3SSI3 <starsm004@gmail.com>
2023-05-10 22:42:06 +0300

    Fix :hammer_and_wrench:

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixx why Aphache is returning a 500 error then automte using puppet.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path   => '/usr/local/bin/:/bin/'
+}
